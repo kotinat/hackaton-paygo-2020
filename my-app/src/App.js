8a78eea616ff10703f97ad39b50e802ea9f5c9f9
Natalia Koti <natalia.meneghetti@paygo.com.br>
2020-09-05 21:15:02 -0300

    fix(app): removing unecessary files and components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,12 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import "./Teste";
-import Teste from "./Teste";
+import React from "react";
 
-function App() {
-  const [cont, setCont] = useState(0);
-
-  const addCont = () => {
-    return setCont(cont);
-  };
-
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Cliquei {cont} vezes.</p>
-        <button onClick={addCont}>+1</button>
-      </header>
-      <Teste />
+      teste fonte
+      <p>teste fonte 2</p>
     </div>
   );
-}
+};
 
 export default App;
