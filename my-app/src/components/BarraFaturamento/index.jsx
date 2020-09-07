d87a6d0a41124f90cf61d9b21005f6fc72b3684b
Henrique Horbovyi <carlos.souza@paygo.com.br>
2020-09-06 22:47:48 -0300

    adiciona menu

--- my-app/src/components/BarraFaturamento/index.jsx
+++ my-app/src/components/BarraFaturamento/index.jsx
@@ -2,33 +2,42 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Faturamento from "../Faturamento";
+import MenuRoundedIcon from "@material-ui/icons/MenuRounded";
 
 const useStyles = makeStyles(() => {
   return {
     root: {
-      height: "180px",
-      padding: "56px 0px 84px 0px",
+      height: "260px",
       display: "flex",
+      paddingBottom: "32px",
       flexDirection: "column",
       justifyContent: "center",
       alignItems: "center",
       backgroundColor: "#242424",
       color: "white",
-      borderBottomRightRadius: "38px",
-      borderBottomLeftRadius: "38px"
+      borderBottomRightRadius: "16px",
+      borderBottomLeftRadius: "16px",
+    },
+    menuIcon: {
+      color: "white",
+      padding: "16px 16px 0 16px"
     },
   };
 });
 
-
 const BarraFaturamento = () => {
-    const classes = useStyles();
-       
-    return (
-        <Box className={classes.root}>
-            <Faturamento valor="R$ 60.000,00" progresso="30" />
+  const classes = useStyles();
+
+  return (
+    <div >
+      <div style={{ textAlign: "left", background: "#242424" }}>
+        <MenuRoundedIcon className={classes.menuIcon} />
+      </div>
+      <Box className={classes.root}>
+          <Faturamento valor="R$ 60.000,00" progresso="30" />
         </Box>
-    );
+    </div>
+  );
 };
 
 export default BarraFaturamento;
--- my-app/src/pages/Objetivos/index.jsx
+++ my-app/src/pages/Objetivos/index.jsx
@@ -4,15 +4,17 @@ import MenuInferior from "../../components/MenuInferior";
 import Objetivo from "../../components/Objetivo";
 import BotaoNovoObjetivo from "../../components/BotaoNovoObjetivo";
 import Titulo from "../../components/Titulo";
+import MenuRoundedIcon from "@material-ui/icons/MenuRounded";
 
 const Objetivos = () => {
   const novoObjetivo = () => {
-    alert("Novo objetivo")
-  }
+    alert("Novo objetivo");
+  };
 
   return (
     <body>
-      <header>
+      <header style={{ display: "flex", alignItems: "center" }}>
+        <MenuRoundedIcon style={{ padding: "16px 0 0 16px" }} />
         <Titulo texto="Meus Objetivos" />
       </header>
       <main>
@@ -22,31 +24,28 @@ const Objetivos = () => {
             data="em 31 de Março de 2021"
             total="10.000,00"
             valorAtual="1.200,00"
-            tipo="Pessoal"  
-
+            tipo="Pessoal"
           />
           <Objetivo
             nome="Ar Condicionado"
             data="em 27 de Dezembro de 2020"
             total="4.000,00"
             valorAtual="2.200,00"
-            tipo="Profissional"  
+            tipo="Profissional"
           />
           <Objetivo
             nome="Mesa de escretório"
             data="em 5 de Outubro de 2020"
             total="1.000,00"
             valorAtual="850,00"
-            tipo="Profissional"  
-
+            tipo="Profissional"
           />
           <Objetivo
             nome="PC pro Bernardo"
             data="em 8 de Janeiro de 2021"
             total="7.000,00"
             valorAtual="2.300,00"
-            tipo="Pessoal"  
-
+            tipo="Pessoal"
           />
         </div>
 
